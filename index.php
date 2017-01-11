4efaa1535418cd18c3f49b1a4b7690017f31e0a6
Charles Denault <charles@charlesdenault.com>
2017-01-11 18:08:58 -0500

    updated images

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Charles Denault III. Founder & CEO at SimpleCharters.com">
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+
+    <title>Charles Denault III - Founder &amp; CEO at SimpleCharters</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link rel="author" href="https://plus.google.com/108694520821234031921"/>
+    <meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Charles Denault III - Founder &amp; CEO at SimpleCharters" />
+		<meta property="og:url" content="http://charlesdenault.com/" />
+		<meta property="og:site_name" content="Charles Denault III" />
+		<meta name="google-site-verification" content="NKrqbWsMlldk4geq5sq9YOFao9G1IvH3zlenwPfjp78" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-42784832-1', 'charlesdenault.com');
+		  ga('send', 'pageview');
+
+		</script>
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="main">
+      	<img src="assets/img/charles-denault-iii.jpg" class="img-circle img-thumbnail" width="150">
+        <h1>Charles Denault III</h1>
+        <h2 class="lead">Founder &amp; CEO at <a href="https://www.simplecharters.com">SimpleCharters</a></h2>
+        <section class="social">
+        <span class="cta">Follow me on:</span>
+	        <a href="http://www.twitter.com/denault3" target="_blank" title="Charles Denault on Twitter"><span>Twitter</span><i class="fa fa-twitter-square"></i></a>
+	        <a href="http://www.facebook.com/denault" target="_blank" title="Charles Denault on Facebook"><span>Facebook</span><i class="fa fa-facebook-square"></i></a>
+	        <a href="http://www.linkedin.com/in/denault" target="_blank" title="Charles Denault on LinkedIn"><span>LinkedIn</span><i class="fa fa-linkedin-square"></i></a>
+	        <a href="http://www.google.com/+charlesdenault" target="_blank" title="Charles Denault on Google+"><span>Google+</span><i class="fa fa-google-plus-square"></i></a>
+	        <a href="http://www.instagram.com/denault3" target="_blank" title="Charles Denault on Instagram"><span>Instagram</span><i class="fa fa-instagram"></i></a>
+	        <a href="http://www.dribbble.com/denault" target="_blank" title="Charles Denault on Dribbble"><span>Dribbble</span><i class="fa fa-dribbble"></i></a>
+         </section>
+      </div>
+    </div><!-- /.container -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
